a701ca4efc6332cf574ea3d37beddc970c80a02c
ChamikaNiroshan <M.ChamikaNiroshan@gmail.com>
2025-01-26 21:22:21 +0530

    2025-1

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const app =  express();
-const cors = require('cors');
-const port = process.env.PORT || 3001;
-const host = 'localhost';
-const mongoose = require('mongoose');
-const router = require('./router');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3001;
+const host = "localhost";
+const mongoose = require("mongoose");
+const router = require("./router");
 
 app.use(cors());
 app.use(express.json());
@@ -13,22 +13,36 @@ app.use(express.json());
     console.log(req.body);
 }) */
 
-const uri = 'mongodb+srv://gaming:gaming1234@cluster0.cpigsp9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+app.use(
+  cors({
+    origin: ["https://deploy-mern-frontend.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
-const connect = async() =>{
-    try{
-        await mongoose.connect(uri);
-        console.log('connected to MongoDB');
-    }
-    catch(error){
-        console.log('MongoDB connection error', error);
-    }
-}
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://gaming:gaming1234@cluster0.cpigsp9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const connect = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log("connected to MongoDB");
+  } catch (error) {
+    console.log("MongoDB connection error", error);
+  }
+};
 
 connect();
 
-const server = app.listen(port,host, () =>
-    console.log(`Node server is listening to ${server.address().port}`)
+const server = app.listen(port, host, () =>
+  console.log(`Node server is listening to ${server.address().port}`)
 );
 
-app.use('/api',router)
+app.use("/api", router);
+
+app.listen(3001, () => {
+    console.log("Server is Running")
+})
